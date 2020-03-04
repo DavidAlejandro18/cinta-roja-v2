14076ff87167da0e89805250ab0a13be80fedd26
David Tovar <alextovar146@gmail.com>
2020-03-04 01:11:26 -0600

    Se modifican los callbacks

--- cinta-roja-v2/exercisesCallbacks.js
+++ cinta-roja-v2/exercisesCallbacks.js
@@ -19,25 +19,41 @@ message("Baia Baia", (mensaje) => {
 });
 
 //Ejercicio 3
-const suma = (num1, num2, callback) => {
+const numeros = (num1, num2, callback) => {
     callback(num1, num2);
 }
 
-suma(4, 4, (num1, num2) => {
+const suma = (num1, num2) => {
     console.log("Suma:", num1 + num2);
+}
+
+const resta = (num1, num2) => {
     console.log("Resta:", num1 - num2);
-    console.log("Multiplicación", num1 * num2);
-});
+}
+
+const multiplicacion = (num1, num2) => {
+    console.log("Multiplicación:", num1 * num2);
+}
+
+numeros(4, 4, suma);
+numeros(12, 3, resta);
+numeros(12, 12, multiplicacion);
 
 //Ejercicio 4
 const ordenSuperior = (cadena, callback) => {
     callback(cadena);
 }
 
-ordenSuperior("Pejelagarto", (cadena) => {
+const mayusculas = (cadena) => {
     console.log("Texto en mayusculas:", cadena.toUpperCase());
+}
+
+const minusculas = (cadena) => {
     console.log("Texto en minusculas:", cadena.toLowerCase());
-});
+}
+
+ordenSuperior("Pejelagarto", mayusculas);
+ordenSuperior("Pejelagarto", minusculas);
 
 //Ejercicio 5
 const tiempo = [120, 400, 200, 100];
